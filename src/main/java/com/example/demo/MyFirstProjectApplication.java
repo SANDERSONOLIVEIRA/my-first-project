6b949fc6459daaba1512c3720c8c5f8a01888a4f
SandersonOliveira <sandersonmoreno@gmail.com>
2018-09-23 20:22:22 -0300

    Alteração no arquivo principal da app.

--- src/main/java/com/example/demo/MyFirstProjectApplication.java
+++ src/main/java/com/example/demo/MyFirstProjectApplication.java
@@ -8,5 +8,6 @@ public class MyFirstProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyFirstProjectApplication.class, args);
+		System.out.println("Meu primeiro projeto ...");
 	}
 }
